a1936e375b4e37b8ba9c2e575e1b5d377f0c321d
16danjj <16danjj@gmail.com>
2024-09-11 21:03:27 +0200

    use a spinlock for multiple threads to access VGA Buffer

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,11 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> !{
+    use core::fmt::Write;
 
-    vga_buffer::print_something();
-        
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers : {}{}", 42, 1.337).unwrap();
+
     loop{}
 } 
  
