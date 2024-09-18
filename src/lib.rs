d2bb7ca3aaba5cad2e7d83d4d575066417cf480b
16danjj <16danjj@gmail.com>
2024-09-18 18:05:32 +0200

    Interrupt handler for INT3

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,12 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 pub mod vga_buffer;
 pub mod serial;
+pub mod interrupts;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -47,6 +49,10 @@ pub extern "C" fn _start() -> !{
 
     loop{}
 } 
+
+pub fn init() {
+    interrupts::init_idt();
+}
 
 #[cfg(test)]
 #[panic_handler]
