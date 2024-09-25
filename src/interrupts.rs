5f15012d69d34471d9f471110e595268c81e331f
16danjj <16danjj@gmail.com>
2024-09-25 13:05:15 +0200

    Introduction to paging

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,8 +1,9 @@
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 use crate::{gdt, println, print};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use spin;
+use crate::hlt_loop;
 
 #[derive(Debug, Clone, Copy)]
 #[repr(u8)]
@@ -34,6 +35,7 @@ lazy_static! {
         }
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt 
     };
 }
@@ -50,6 +52,15 @@ extern "x86-interrupt" fn double_fault_handler (stack_frame : InterruptStackFram
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
+extern "x86-interrupt" fn page_fault_handler(stack_frame : InterruptStackFrame, error_code: PageFaultErrorCode) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
+}
 
 
 #[test_case]
